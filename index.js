c23814cf9e36efba1841f26879f7291ad249caf8
saifullah022 <sheikhsaifullah022@gmail.com>
2024-07-29 23:15:44 +0600

    add some comments

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 //js code
 function Stopwatch() {
     let runtime, duration, starttime, endtime = 0;
-    this.start = function() {
+    this.start = function() {     // for start time
       if(runtime)
         throw new Error('StopWatch already started');
       runtime = true;
       starttime = new Date();
     };
-    this.stop = function() {
+    this.stop = function() {      // stope time
       if(!runtime)
         throw new Error('stopWatch already stoped');
       runtime = false;
       endtime = new Date();
       duration = (endtime.getTime() - starttime.getTime()) /1000;
     };
-    this.reset = function() {
+    this.reset = function() {       // for reset all
       runtime = false;
       duration = 0;
       starttime = null;
       endtime = null;
     };
-    Object.defineProperty(this,'duration',{
+    Object.defineProperty(this,'duration',{      // convert duration read only property
       get: function() {
         return duration;
       }
